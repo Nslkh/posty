a44204465392d01babc801e8f4e5a39c0609999c
Nslkh <olluzran@gmail.com>
2021-03-18 13:48:19 +0500

    Main Page Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Posty</title>
-    <link rel="sytlesheet" href="{{ asset('css/app.css')}}">
+    <link rel="sytlesheet" href="{{ asset('css/app.css') }}">
   </head>
-  <body>
+  <body class="bg-gray-100">
+    <nav class="p-6 bg-white flex justify-between">
+      <ul class="flex items-center">
+        <li>
+          <a href="" class="p-3">Home</a>
+        </li>
+        <li>
+          <a href="" class="p-3">Dashboard</a>
+        </li>
+        <li>
+          <a href="" class="p-3">Post</a>
+        </li>
+      </ul>
+
+      <ul class="flex items-center">
+        <li>
+          <a href="" class="p-3">Narzullo Salakhidinov</a>
+        </li>
+        <li>
+          <a href="" class="p-3">Login</a>
+        </li>
+        <li>
+          <a href="" class="p-3">Register</a>
+        </li>
+        <li>
+          <a href="" class="p-3">Logout</a>
+        </li>
+      </ul>
+    </nav>
     @yield('content')
   </body>
 </html>
