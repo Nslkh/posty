ca2f5fa63915948fe891198ba3b97962210b15c4
Nslkh <olluzran@gmail.com>
2021-03-19 07:39:21 +0500

    Register Page Form Created

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="flex justify-center">
+    <div class="w-4/12 bg-white p-6 rounded-lg">
+     <form action=" {{ route ('register') }}" method="post">
+        @csrf
+        <div class="mb-4">
+          <label for="name" class="sr-only">Name</label>
+          <input type="text" name="name" id="name" placeholder="Your Name" 
+          class="bg-gray-100 border-2 w-full p-4 rpunded-lg" value="">
+        </div>
+
+        <div class="mb-4">
+          <label for="username" class="sr-only">Username</label>
+          <input type="text" name="username" id="username" placeholder="Username" 
+          class="bg-gray-100 border-2 w-full p-4 rpunded-lg" value="">
+        </div>
+
+        <div class="mb-4">
+          <label for="name" class="sr-only">Email</label>
+          <input type="text" name="name" id="email" placeholder="Your Email" 
+          class="bg-gray-100 border-2 w-full p-4 rpunded-lg" value="">
+        </div>
+
+        <div class="mb-4">
+          <label for="password" class="sr-only">PASSWORD</label>
+          <input type="password" name="password" id="password" placeholder=" Choose a Password" 
+          class="bg-gray-100 border-2 w-full p-4 rpunded-lg" value="">
+        </div>
+
+        <div class="mb-4">
+          <label for="password_confirmation" class="sr-only">Password Again</label>
+          <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Repeat your Password" 
+          class="bg-gray-100 border-2 w-full p-4 rpunded-lg" value="">
+        </div>
+
+        <div>
+          <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Register</button>
+        </div>
+     </form>
+    </div>
+  </div>
+@endsection
