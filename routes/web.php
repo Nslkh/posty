d135646003499fd40050946f48cd9b6127026b71
Nslkh <olluzran@gmail.com>
2021-03-19 22:17:59 +0500

    Post routing

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -35,7 +36,7 @@ Route::post('/login', [LoginController::class, 'store']);
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+
+
