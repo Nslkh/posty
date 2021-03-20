0a41f84ba620f8d76c01833f494ba4421d0ced9c
Nslkh <olluzran@gmail.com>
2021-03-20 09:11:08 +0500

    Listing Post finished

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -12,7 +13,8 @@ class DashboardController extends Controller
     }
     public function index()
     {
-        // dd(auth()->user()->posts);
+        // dd(Post::find(4)->created_at);
+        // // dd(auth()->user()->posts);
         return view('dashboard');
     }
 }
