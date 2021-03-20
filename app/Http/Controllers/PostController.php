765e1e8d40f1f84e429d0777292ddcbe87595a72
Nslkh <olluzran@gmail.com>
2021-03-20 13:55:33 +0500

    Pagination implemented

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,8 @@ class PostController extends Controller
     public function index()
     {
 
-        $posts = Post::get();
+        $posts = Post::paginate(2);
+
         return view('posts.index',[
             'posts' => $posts
         ]);
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -31,6 +31,8 @@
             <p class="mb-2">{{ $post->body}}</p>        
           </div>
         @endforeach  
+
+        {{ $posts->links()}}
       @else
         <p>There are no posts</p>
       @endif        
