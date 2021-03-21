8c4ab2286c43a8c294cf87b1eb36446f6760e5c8
Nslkh <olluzran@gmail.com>
2021-03-21 07:31:35 +0500

    Final tidying

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,29 +1,28 @@
-
-
 @extends('layouts.app')
 
 @section('content')
   <div class="flex justify-center">
     <div class="w-8/12 bg-white p-6 rounded-lg">
-      <form action="{{ route('posts') }}" method="post" class="mb-4">
-        @csrf
-        <div class="mb-4">
-          <label for="body" class="sr-only"></label>
-          <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" 
-          placeholder="Post something!"></textarea>
+      @auth
+        <form action="{{ route('posts') }}" method="post" class="mb-4">
+          @csrf
+          <div class="mb-4">
+            <label for="body" class="sr-only"></label>
+            <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" 
+            placeholder="Post something!"></textarea>
 
-          @error('body')
-            <div class="text-red-500 mt-2 text-sm">
-              {{ $message}}
-            </div>
-          @enderror  
-        </div>
-        
-        <div class="">
-          <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
-        </div>
-
-      </form>
+            @error('body')
+              <div class="text-red-500 mt-2 text-sm">
+                {{ $message}}
+              </div>
+            @enderror  
+          </div>
+          
+          <div class="">
+            <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
+          </div>
+        </form>
+      @endauth
 
       @if ($posts->count())
         @foreach ($posts as $post)
