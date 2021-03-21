0bb3e0c5e0e720cf0f4378883e083926bed4e78c
Nslkh <olluzran@gmail.com>
2021-03-21 07:04:29 +0500

    Sending email

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\PostLiked;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class DashboardController extends Controller
 {
@@ -13,6 +15,7 @@ class DashboardController extends Controller
     }
     public function index()
     {
+        
         // dd(Post::find(4)->created_at);
         // // dd(auth()->user()->posts);
         return view('dashboard');
